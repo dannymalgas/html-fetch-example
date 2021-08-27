e4b638e25ebee1be2388c71450489aa98d3ffe9d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-08-27 09:13:50 +0200

    Update fetch.js

--- front/scripts/fetch.js
+++ front/scripts/fetch.js
@@ -1,20 +1,19 @@
 const API_URL = "http://localhost:3000"
 
-const getData = async () => {
-    // Make the call to the endpoint.
-    const resp = await fetch(API_URL)
+const getData = async (url) => {
+    // Request an endpoint.
+    const resp = await fetch(url)
 
     if (!resp.ok()) {
       throw new Error(`${resp.statusCode}`)
     }
-    const data = resp.json()
+
+    const data = await resp.json()
     
     const el = document.getElementById("message")
-  
     // Replace the HTML content with the fetched data.
     el.innerHTML = data.message
 }
 
 console.log("Fetching data");
-
-getData();
+getData(API_URL);
